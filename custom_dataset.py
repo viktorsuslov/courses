597faab0dec4b5d02fefdbec30596f38961fd665
viktorsuslov <73917381+viktorsuslov@users.noreply.github.com>
2024-06-03 23:26:03 +0300

    Update custom_dataset.py

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,15 +1,16 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+
 class CustomDataset(Dataset):
-    
+
     def __init__(self, X, y):
         self.X = X
         self.y = y
-        
+
     def __len__(self):
         return len(self.X)
-    
+
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
 
