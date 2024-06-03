1f32f471d5f4b03694ead829a3a57265be143ae8
sure_networks <viktor@MacBook-Pro-Viktor.local>
2024-06-03 21:18:27 +0300

    test torch

---
+++ torch.py
@@ -0,0 +1,28 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+X = torch.linspace(0, 1, 100).view(-1, 1)
+y = torch.sin(2 * torch.pi * X) + 0.1 * torch.rand(X.size())
+
+dataset = CustomDataset(X, y)
+dataloader = DataLoader(dataset, batch_size=10)
+
+
+for batch in dataloader:
+    data, labels = batch
+    print("Batch data shape:", data.shape)
+    print("Batch labels shape:", labels.shape)
+    print("Data:", data)
+    print("Labels:", labels)
+    break  # Выход
